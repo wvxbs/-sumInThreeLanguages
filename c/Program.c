2302e3cafce19a124a005b078c0a23ab8ee5fd2b
wvxbs <gabriel.ferreira7854@gmail.com>
2020-04-24 09:28:27 -0300

    .

--- c/Program.c
+++ c/Program.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 #include <time.h>
 
-void SumNumbers(long long firstNumber, long long finalNumber);                               
-void calculateTime(clock_t initial, clock_t final);
+long long sumNumbers(long long firstNumber, long long finalNumber);                               
+double calculateTime(clock_t initial, clock_t final);
 
 void main() {
-    SumNumbers(0, 1000000);
-}
- 
-void SumNumbers (long long firstNumber, long long finalNumber) {
 
     clock_t initial, final;
 
     initial = clock();
 
+    long long result = sumNumbers(0, 1000000);
+    printf("\nResultado: %lld", result);
+
+    final = clock();
+
+    double time = calculateTime(initial, final);
+
+    printf("\nTempo decorrido: %f milissegundos", time);
+}
+ 
+long long sumNumbers (long long firstNumber, long long finalNumber) {
     for(int i = 0; i <= finalNumber; i++) {
         firstNumber = firstNumber+ i;
     }
 
-    final = clock();
-
-    printf("\nResultado: %lld", firstNumber);
-
-    calculateTime(initial, final);
+    return firstNumber;
 }
 
-void calculateTime (clock_t initial, clock_t final) {
-    double time = final - initial / CLOCKS_PER_SEC;
+double calculateTime (clock_t initial, clock_t final) {
+    double Time = final - initial / CLOCKS_PER_SEC;
 
-    printf("\nTempo decorrido: %f milissegundos", time);
+    return Time;
 }
